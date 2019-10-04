0aed6a103b314ad7664eebbad1e850b7f6fa35c4
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-04 08:57:25 +0100

    render playerCard

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="App">
-        
+        <PlayerCard player={this.state.playerDetails}/>
       </div>
     );
   }

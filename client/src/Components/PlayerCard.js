759ea66d17882bed74b28f268aab339d32294711
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-04 10:05:02 +0100

    use locall storage for country

--- client/src/Components/PlayerCard.js
+++ client/src/Components/PlayerCard.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { PlayerStyle } from './Styles';
 import { LocalStorage } from '../Hooks/LocalStorage';
 
-export default function PlayerCard (props) {
+export default function PlayerCard(props) {
     const { name, country, searches } = props.player;
-    const [playerName, setPlayerName] = LocalStorage("name", "")
+    const [playerName, setPlayerName] = LocalStorage('name', "");
+    const [countryName, setCountryName] = LocalStorage('country', 'value')
     return (
-        <PlayerStyle onClick={() => setPlayerName(name)}>
+        <PlayerStyle onClick={() => {
+            setPlayerName(name);
+            setCountryName(country);
+        }}>
             <h2>Name: {name}</h2>
             <p>Country: {country}</p>
             <p>Searches: {searches}</p>

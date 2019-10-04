a8397ec8828492ca8d2a2ec7512d1ba5f97fe406
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-04 09:50:26 +0100

    create custom localStorage

--- client/src/Components/PlayerCard.js
+++ client/src/Components/PlayerCard.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { PlayerStyle } from './Styles';
+import { LocalStorage } from '../Hooks/LocalStorage';
 
-export default class PlayerCard extends React.Component {
-    render () {
-        const { name, country, searches } = this.props.player;
-        return (
-            <PlayerStyle>
-                <h2>Name: {name}</h2>
-                <p>Country: {country}</p>
-                <p>Searches: {searches}</p>
-            </PlayerStyle>
-        );
-    }
+export default function PlayerCard (props) {
+    const { name, country, searches } = props.player;
+
+    return (
+        <PlayerStyle>
+            <h2>Name: {name}</h2>
+            <p>Country: {country}</p>
+            <p>Searches: {searches}</p>
+        </PlayerStyle>
+    );
 }
--- client/src/Hooks/LocalStorage.js
+++ client/src/Hooks/LocalStorage.js
@@ -5,13 +5,13 @@ export const UseLocalStorage = (key, initialValue) => {
     const [storedValue, setStoredValue] = useState(() => {
         const item = window.localStorage.getItem(key);
         return item 
-            ? JSON.parse(item) 
+            ? item
             : initialValue;
     });
 
     const setValue = value => {
         setStoredValue(value);
-        window.localStorage.setItem(key, JSON.stringify(value));
+        window.localStorage.setItem(key, (value));
     };
 
     return [storedValue, setValue];

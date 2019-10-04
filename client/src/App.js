73fb6587920dbde091354d0dee976f474d40cf44
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-04 08:48:11 +0100

    fetch data from localhost using axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
+const playerApi = 'http://localhost:5000/api/players';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      playerDetails: {},
     };
+  }
+
+  componentDidMount() {
+    axios.get(playerApi)
+      .then(res => {
+        console.log(res.data)
+        this.setState({ playerDetails: res.data });
+      })
+      .catch(err => {
+        console.log(err.message);
+      })
   }
 
   render() {

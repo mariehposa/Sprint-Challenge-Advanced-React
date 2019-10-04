d4eb867f38d6759af5455428b6e1583a577cf9c3
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-04 10:38:02 +0100

    create test for correct user and searches in playcard

---
+++ client/src/Components/__tests__/PlayerCard.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import * as rtl from '@testing-library/react';
+import PlayerCard from '../PlayerCard';
+
+let tools;
+
+beforeEach(() => {
+    rtl.cleanup();
+
+    const player = {
+        name: "Maryam Adedeji",
+        id: 123,
+        country: "Nigeria",
+        searches: "50"
+    }
+
+    tools = rtl.render(<PlayerCard player={player} />)
+});
+
+describe('PlayerCard Component', () => {
+    it('can debug the output', () => {
+        //tools.debug();
+    });
+
+    it('shows the correct user', () => {
+        const elementWithMaryam = tools.queryByText(/Maryam/i)
+        expect(elementWithMaryam).toBeInTheDocument();
+    })
+
+    it('shows correct searches', () => {
+        const elementWithSearches = tools.queryByText(/50/i);
+        expect(elementWithSearches).toBeInTheDocument();
+      });
+})
